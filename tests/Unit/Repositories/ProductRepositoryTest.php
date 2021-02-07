1888573c8193e57c83f80697c061f117388a71b1
Nguyen Tu Quy <tu.nguyen@unitedprint.com>
2021-02-07 20:01:02 +0100

    added test ProductRepositoryTest.php

--- tests/Unit/Repositories/ProductRepositoryTest.php
+++ tests/Unit/Repositories/ProductRepositoryTest.php
@@ -23,6 +23,88 @@ class ProductRepositoryTest extends TestCase
         $repository = app(ProductRepository::class);
         $repository->addTags($product, [$tag->id]);
 
-        $this->assertGreaterThan(0, $product->tags()->count());
+        $this->assertGreaterThan(0, $product->tags->count());
+    }
+
+    /**
+     * @author Admin <admin@gmail.com>
+     */
+    public function testGet()
+    {
+        $product = Product::factory()->create();
+        $repository = app(ProductRepository::class);
+        $model = $repository->get();
+        $this->assertGreaterThan(0, $product->count());
+    }
+
+    /**
+     * @author Admin <admin@gmail.com>
+     */
+    public function testFind()
+    {
+        $product = Product::factory()->create();
+        $repository = app(ProductRepository::class);
+        $product_ = $repository->find($product->id);
+        $this->assertGreaterThan(0, $product_->count());
+    }
+
+    /**
+     * @author Admin <admin@gmail.com>
+     */
+    public function testCreate()
+    {
+        $repository = app(ProductRepository::class);
+        $product = $repository->create([
+            'name' => "abc",
+            'description' => "abc",
+            'price' => 100,
+            'category_id' => 1,
+            'photo' => 'images/cars/cima_1912_top_01.jpg.ximg.l_full_m.smart.jpg',
+            ]
+        );
+        $product_ = $repository->find($product->id);
+        $this->assertEquals($product->id, $product_->id);
+    }
+
+
+    /**
+     * @author Admin <admin@gmail.com>
+     */
+    public function testUpdate()
+    {
+        $product = Product::factory()->create();
+        $repository = app(ProductRepository::class);
+        $product = $repository->update($product->id, [
+                'name' => "abc",
+                'description' => "abc",
+                'price' => 100,
+                'category_id' => 1,
+                'photo' => 'images/cars/cima_1912_top_01.jpg.ximg.l_full_m.smart.jpg',
+            ]
+        );
+        $product_ = $repository->find($product->id);
+        $this->assertEquals($product->abc, $product_->abc);
+    }
+
+    /**
+     * @author Admin <admin@gmail.com>
+     */
+    public function testDelete()
+    {
+        $product = Product::factory()->create();
+        $repository = app(ProductRepository::class);
+        $product = $repository->delete($product->id);
+        $this->assertEquals(true, $product);
+    }
+
+    /**
+     * @author Admin <admin@gmail.com>
+     */
+    public function testShow()
+    {
+        $product = Product::factory()->create();
+        $repository = app(ProductRepository::class);
+        $product_ = $repository->show($product->id);
+        $this->assertEquals($product->name, $product_->name);
     }
 }
